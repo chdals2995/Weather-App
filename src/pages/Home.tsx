f9a0c5f17ab0910c9d4c5055d704cd8bff1fc4f3
chdals2995 <chdals2995@gmail.com>
2026-01-08 19:24:05 +0900

    feat:홈 레이아웃 대략적

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+
+export default function Home(){
+    return(
+        <div>
+            {/* 최상단 메뉴 */}
+            <section className="flex">
+                <div>현위치</div>
+                <div>즐겨찾기</div>
+            </section>
+            {/* 상단 */}
+            <section className=""></section>
+            {/* 검색 */}
+            <section className=""></section>
+            {/* 하단 */}
+            <section className=""></section>
+        </div>
+    );
+}
