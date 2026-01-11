ad66cee6e6c97c3fab43b8208e81ca325eaa612d
chdals2995 <chdals2995@gmail.com>
2026-01-12 04:30:25 +0900

    현재 날씨 기능 완성

--- src/shared/weather.ts
+++ src/shared/weather.ts
@@ -1,5 +1,7 @@
+// Weather.ts (API)
+
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
-const BASE_URL = "https://api.openweathermap.org/data";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
 // openweathermap api기본 주소 - 유지보수를 위해 공통 도메인 분리
 
 if (!API_KEY) {
@@ -11,7 +13,7 @@ export async function getCurrentByCoord(lat: number, lon: number ) {
 
   const res = await fetch(
 
-    `${BASE_URL}/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
+    `${BASE_URL}/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
   );    // 위도(lat), 경도(lon), 섭씨 단위(metric), API 키
 
   if (!res.ok) {
@@ -24,7 +26,7 @@ export async function getCurrentByCoord(lat: number, lon: number ) {
 // 도시명 기준 현재 날씨 (검색 / 즐겨찾기용)
 export async function getCurrentByCity(city: string) {
   const res = await fetch(
-    `${BASE_URL}/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+    `${BASE_URL}/weather?q=${city}&units=metric&appid=${API_KEY}`
   );
 
   if (!res.ok) {
@@ -34,14 +36,27 @@ export async function getCurrentByCity(city: string) {
   return res.json();
 }
 
-// 위치 기준 시간대별 날씨
-export async function getHourlyByCoord(lat: number, lon: number) {
+// 3시간 단위 예보 (최대 5일치)
+export async function getForecastByCoord(lat: number, lon: number) {
   const res = await fetch(
-    `${BASE_URL}/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,alerts&appid=${API_KEY}`
-  );    // exclude: 필요 없는 데이터 제외 (용량 줄이기)
+    `${BASE_URL}/forecast?&lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
+  );
 
   if (!res.ok) {
     throw new Error("시간대별 날씨 조회 실패");
+  }
+
+  return res.json();
+}
+
+// 도시명 기준 3시간 단위 예보 (검색/즐겨찾기)
+export async function getForecastByCity(city: string) {
+  const res = await fetch(
+    `${BASE_URL}/forecast?q=${city}&units=metric&appid=${API_KEY}`
+  );
+
+  if (!res.ok) {
+    throw new Error("도시별 시간대별 날씨 조회 실패");
   }
 
   return res.json();
